029e2a86cf0cca3f308d847b95040673b6481978
isabelxklee <hello@isabelklee.co>
2021-01-04 18:19:18 -0500

    changed header style

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 const Navigation = styled.header`
-  background-color: #FBEDF1;
+  background: linear-gradient(180deg, rgba(251,237,241,1) 50%, rgba(251,237,241,0) 100%);
   top: 0;
-  height: 4rem;
+  height: 7rem;
   margin: auto;
-  padding: 1rem 3rem;
+  padding: 1rem 3rem 0;
 
   @media only screen and (max-width: 700px) {
     height: auto;
