551ac28ab079eda7e19e11d826e03558b2b0be01
isabelxklee <hello@isabelklee.co>
2021-01-05 15:45:45 -0500

    refactored list item component

--- src/components/PanelItem.jsx
+++ src/components/PanelItem.jsx
@@ -1,18 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const ListItem = styled.article`
-  padding: 1.4rem 2rem;
-  cursor: pointer;
-  transition: 0.3s;
-  border-bottom: 3px solid #ea4a4a;
-  &:hover {
-    background-color: #dadada;
-  }
-  @media only screen and (max-width: 700px) {
-    padding: 1rem 1.8rem;
-  }
-`
+import ListItem from './ListItem.jsx'
 
 const PanelItem = ({place, map}) => {
   const flyToLocation = () => {
