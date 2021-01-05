243ee3a21cbec411597f801293e4de00ee762999
isabelxklee <hello@isabelklee.co>
2021-01-05 15:41:18 -0500

    refactored panel component

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,28 +1,14 @@
-import React, {Component} from 'react'
-import styled from 'styled-components'
+import React from 'react'
 import PanelChild from './PanelChild.jsx'
 
-const List = styled.ul`
-  list-style: none;
-`
-
-class Panel extends Component {
-  render() {
-    const places = this.props.places
-    let placesList = []
-
-    if (places) {
-      placesList = places.map((place, index) => {
-        return <PanelChild key={index} place={place} map={this.props.map} />
-      })
-    }
-
-    return (
-      <section className="panel">
-        <List>{placesList}</List>
-      </section>
-    )
-  }
-}
+const Panel = ({places, map}) => (
+  <section className="panel">
+    <ul>
+      {places.map((place, index) => (
+        <PanelChild key={index} place={place} map={map} />
+      ))}
+    </ul>
+  </section>
+)
 
 export default Panel
