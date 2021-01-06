5b42c71c21c9242b1b40191c7c119759ebef17b8
isabelxklee <hello@isabelklee.co>
2021-01-06 14:12:11 -0500

    added componentDidUpdate

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -24,27 +24,31 @@ class Map extends Component {
     this.props.setMap(map)
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.places !== this.props.places) {
+      this.props.places.map((place) => {
+        const marker = new mapboxgl.Marker({
+          color: '#ea4a4a',
+        })
+        marker.setLngLat(place.coordinates)
+        marker.addTo(this.props.map)
+  
+        const popup = new mapboxgl.Popup({offset: 0}).setHTML(
+          `<h3 class="popup"><a href=${place.website} target="noreferrer_blank" class="popup">${place.title}</a></h3>
+                <p class="popup">${place.address}</p>
+                <p class="popup">${place.city}, ${place.state}</p>`
+        )
+  
+        marker.setPopup(popup)
+      })
+    }
+  }
+
   // TODO:
   // 1. do we need to keep updating the markers each time?
   // 2. should we keep track of markers to remove old ones if we update?
 
   render() {
-    this.props.places.map((place) => {
-      const marker = new mapboxgl.Marker({
-        color: '#ea4a4a',
-      })
-      marker.setLngLat(place.coordinates)
-      marker.addTo(this.props.map)
-
-      const popup = new mapboxgl.Popup({offset: 0}).setHTML(
-        `<h3 class="popup"><a href=${place.website} target="noreferrer_blank" class="popup">${place.title}</a></h3>
-              <p class="popup">${place.address}</p>
-              <p class="popup">${place.city}, ${place.state}</p>`
-      )
-
-      marker.setPopup(popup)
-    })
-
     return (
       <section>
         <PlacesList places={this.props.places} map={this.props.map} />
@@ -57,7 +61,7 @@ class Map extends Component {
 Map.propTypes = {
   places: IBusinesses.isRequired,
   app: IMapState.isRequired,
-  map: IMapState.isRequired,
+  map: IMapState,
   setMap: func.isRequired,
 }
 
