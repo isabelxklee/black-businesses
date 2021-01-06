1746753f8eb041e716161d1290c64f5ab816d30d
isabelxklee <hello@isabelklee.co>
2021-01-06 13:45:06 -0500

    changed App back to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {Component} from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import GlobalStyle from './components/styled-components/GlobalStyle.jsx'
 import Map from './components/Map.jsx'
@@ -7,54 +7,58 @@ import Showcase from './components/Showcase.jsx'
 import Resources from './components/Resources.jsx'
 import BusinessPage from './components/BusinessPage.jsx'
 
-const App = () => {
-  const [mapState, setMapState] = useState({
+class App extends Component {
+  state = {
     long: -101.20869,
     lat: 39.8383872,
     zoom: 3.5,
-    map: null,
     style: 'mapbox://styles/mapbox/light-v9',
-  })
-  const [places, setPlaces] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('https://black-businesses-json.herokuapp.com/places')
-      const places = await response.json()
-      setPlaces(places)
-    }
-
-    fetchData()
-  }, [])
-
-  const updateMap = (newMap) => {
-    setMapState((mapState) => ({...mapState, map: newMap}))
+    places: [],
+    map: null,
   }
 
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <section>
-        <Switch>
-          <Route exact path="/">
-            <Map mapState={mapState} places={places} setMap={updateMap} />
-          </Route>
-          <Route exact path="/businesses">
-            <Showcase businesses={places} />
-          </Route>
-          <Route exact path="/resources">
-            <Resources />
-          </Route>
-          {places.map((business) => (
-            <Route exact path={`/businesses/${business.id}`} key={business.id}>
-              <BusinessPage business={business} />
+  componentDidMount() {
+    fetch('https://black-businesses-json.herokuapp.com/places')
+      .then((response) => response.json())
+      .then((placesArray) => {
+        this.setState({
+          places: placesArray,
+        })
+      })
+  }
+
+  setMap = (inputFromChild) => {
+    this.setState({
+      map: inputFromChild,
+    })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <section>
+          <Switch>
+            <Route exact path="/">
+              <Map map={this.state.map} places={this.state.places} setMap={this.setMap} />
             </Route>
-          ))}
-        </Switch>
-      </section>
-    </BrowserRouter>
-  )
+            <Route exact path="/businesses">
+              <Showcase businesses={this.state.places} />
+            </Route>
+            <Route exact path="/resources">
+              <Resources />
+            </Route>
+            {this.state.places.map((business) => (
+              <Route exact path={`/businesses/${business.id}`} key={business.id}>
+                <BusinessPage business={business} />
+              </Route>
+            ))}
+          </Switch>
+        </section>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App
