827c1b3c8694978e5e3c287f9f78002afeccdc52
isabelxklee <hello@isabelklee.co>
2021-01-08 12:11:14 -0500

    installed redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+const store = createStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 )
