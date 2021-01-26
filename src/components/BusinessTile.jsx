70589d90496915306b722269aa5bb2e8a10ad91c
isabelxklee <hello@isabelklee.co>
2021-01-25 19:48:33 -0500

    updated secondary tag style

--- src/components/BusinessTile.jsx
+++ src/components/BusinessTile.jsx
@@ -10,22 +10,18 @@ const BusinessTile = ({business}) => (
     <Link to={`businesses/${business.id}`}>
       <img src={business.image_url} alt={business.title} className="business-tile-image" />
     </Link>
-    <div className="business-tile-info">
-      <div className="category-container">
-        <BusinessTags business={business} />
-      </div>
-      <Link to={`businesses/${business.id}`}>
-        <h3 className="business-tile-name">{business.title}</h3>
-      </Link>
-      <img src={mapPin} alt="red icon of a map pin" className="map-pin" />
-      <p className="address">
-        {business.city}, {business.state}
-      </p>
-      <p className="description">{business.description}</p>
-      <a href={business.website} target="noreferrer_blank" className="site-link">
-        Visit their site
-      </a>
-    </div>
+    <BusinessTags business={business} />
+    <Link to={`businesses/${business.id}`}>
+      <h3 className="business-tile-name">{business.title}</h3>
+    </Link>
+    <img src={mapPin} alt="red icon of a map pin" className="map-pin" />
+    <p className="address">
+      {business.city}, {business.state}
+    </p>
+    <p className="description">{business.description}</p>
+    <a href={business.website} target="noreferrer_blank" className="site-link">
+      Visit their site
+    </a>
   </BusinessTileCard>
 )
 
