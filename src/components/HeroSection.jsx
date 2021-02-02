be29187c51c6727d74cec2682bf4f1ca94c8fa7d
isabelxklee <hello@isabelklee.co>
2021-02-02 12:06:16 -0500

    updated styles for hero text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {ReactComponent as TileDivider} from '../assets/tile-divider.svg'
 import HeroGrid from './styled-components/HeroGrid.jsx'
 import HeroTextContainer from './styled-components/HeroTextContainer.jsx'
-import HeroTextH2 from './styled-components/HeroTextH2.jsx'
+import HeroText from './styled-components/HeroText.jsx'
+import H2 from './styled-components/H2.jsx'
 import heroImage from '../assets/hero-image.png'
 import HeroImage from './styled-components/HeroImage.jsx'
 import P from './styled-components/P.jsx'
@@ -12,11 +13,13 @@ const HeroSection = () => (
     <HeroGrid>
       <HeroImage src={heroImage} />
       <HeroTextContainer>
-        <HeroTextH2>Support local Black owned businesses!</HeroTextH2>
-        <P>
-          Use our directory to discover new Black owned businesses local to you, or add your own to
-          the directory.
-        </P>
+        <HeroText>
+          <H2>Support local Black owned businesses!</H2>
+          <P>
+            Use our directory to discover new Black owned businesses local to you, or add your own
+            to the directory.
+          </P>
+        </HeroText>
       </HeroTextContainer>
     </HeroGrid>
 
