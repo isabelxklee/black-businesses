2d947485bba995a3d1371642ecc37c1ab43bb2f3
isabelxklee <hello@isabelklee.co>
2021-02-02 12:15:34 -0500

    fixed grid spacing for showcase page

--- src/components/styled-components/BusinessTileCard.jsx
+++ src/components/styled-components/BusinessTileCard.jsx
@@ -1,10 +1,7 @@
 import styled from 'styled-components'
 
 const BusinessTileCard = styled.div`
-  width: 85%;
-
   @media (max-width: 700px) {
-    width: 100%;
     border-bottom: 3px solid #ce1e20;
     padding: 16px 0 40px 0;
   }
