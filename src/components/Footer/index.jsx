1a411371994678325d042f1a7d7d83e5e97329de
Ryan <ryan@artcommaco.de>
2021-02-04 15:10:30 +0000

    fix for footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import {Message} from './styles'
+import {MessageWrapper, Message} from './styles'
 import {H3, P2} from '../../styles'
 import {LargeCheck} from '../Check/styles'
 
 const Footer = () => (
   <footer>
     <LargeCheck />
-    <Message>
-      <H3>Have a website to add to the directory?</H3>
-      <P2>
-        If you’d like to submit a business, whether it’s your own or your favourite place to grab
-        your lunch, email us at submit@black-owned-businesses.com
-      </P2>
-    </Message>
+    <MessageWrapper>
+      <Message>
+        <H3>Have a website to add to the directory?</H3>
+        <P2>
+          If you’d like to submit a business, whether it’s your own or your favourite place to grab
+          your lunch, email us at submit@black-owned-businesses.com
+        </P2>
+      </Message>
+    </MessageWrapper>
     <LargeCheck />
   </footer>
 )
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,17 +1,22 @@
 import styled from 'styled-components'
 
-export const Message = styled.div`
-  padding: 84px 300px;
+export const MessageWrapper = styled.div`
+  padding: 84px 30px;
   margin: 80px 0;
   background-color: #fcdfdd;
-  text-align: center;
 
   @media (max-width: 1000px) {
-    padding: 120px 200px;
+    padding: 120px 30px;
   }
 
   @media (max-width: 700px) {
     padding: 80px 30px;
     margin: 0;
   }
+`
+
+export const Message = styled.div`
+  margin: 0 auto;
+  max-width: 550px;
+  text-align: center;
 `
