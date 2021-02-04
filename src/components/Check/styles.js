722790373f78a8e3e715b02dbcf5f272ce8af663
Ryan <ryan@artcommaco.de>
2021-02-04 15:10:21 +0000

    fix for checks

--- src/components/Check/styles.js
+++ src/components/Check/styles.js
@@ -1,19 +1,20 @@
 import styled from 'styled-components'
 import smallCheckSrc from '../../assets/small-check.svg'
-import largeCheckSrc from '../../assets/small-check.svg'
+import largeCheckSrc from '../../assets/large-check.svg'
 
 const Check = styled.div`
   background-repeat: repeat-x;
-  background-size: contain;
   width: 100%;
 `
 
 export const SmallCheck = styled(Check)`
   background-image: url(${smallCheckSrc});
+  background-size: auto 40px;
   height: 40px;
 `
 
 export const LargeCheck = styled(Check)`
   background-image: url(${largeCheckSrc});
+  background-size: auto 62px;
   height: 62px;
 `
