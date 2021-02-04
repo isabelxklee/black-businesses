69c58e35e9ae4ff5f9284076e7c9f861f48014d7
Ryan <ryan@artcommaco.de>
2021-02-04 15:16:40 +0000

    fix for external links, and add proptypes

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {Link} from './styles'
 
-export const ExternalLink = ({to, children}) => (
-  <Link to={to} target="_blank" rel="noopener noreferrer">
+export const ExternalLink = ({href, children}) => (
+  <Link href={href} target="_blank" rel="noopener noreferrer">
     {children}
   </Link>
 )
+
+ExternalLink.propTypes = {
+  href: PropTypes.string.isRequired,
+}
