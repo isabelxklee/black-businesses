886d36e3443c5b3fb8722374b1d1c2efd2fc6bd9
Ryan <ryan@artcommaco.de>
2021-02-05 09:43:19 +0000

    add email link to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {ExternalLink} from '../Links'
 import {MessageWrapper, Message} from './styles'
 import {H3, P2} from '../../styles'
 import {LargeCheck} from '../Check/styles'
@@ -11,7 +12,8 @@ const Footer = () => (
         <H3>Have a website to add to the directory?</H3>
         <P2>
           If you’d like to submit a business, whether it’s your own or your favourite place to grab
-          your lunch, email us at submit@black-owned-businesses.com
+          your lunch, email us at{' '}
+          <ExternalLink href="mailto:hi@superhi.com">submit@black-owned.com</ExternalLink>
         </P2>
       </Message>
     </MessageWrapper>
